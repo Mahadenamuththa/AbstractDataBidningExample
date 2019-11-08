03d66d2aee57ed4c8894fbffeebc82d86a6d27ec
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2019-11-08 18:29:55 +0530

    Comments

--- ABD.WEB/Models/ErrorViewModel.cs
+++ ABD.WEB/Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace ABD.WEB.Models
 {
     public class ErrorViewModel
     {
+        //hi
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
